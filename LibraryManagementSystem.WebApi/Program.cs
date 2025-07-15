4e6e97fbd2d22dbbf7f4152187dcda740ebf9a2d
Gideon Isa <innocentgideon10@gmail.com>
2025-07-15 09:22:48 +0100

    feat(api-versioning): add versioning

--- LibraryManagementSystem.WebApi/Program.cs
+++ LibraryManagementSystem.WebApi/Program.cs
@@ -1,6 +1,9 @@
 using LibrarayManagementSystem.Application;
 using LibraryManagementSystem.Infrastructure;
 using LibraryManagementSystem.Infrastructure.Database.Data;
+using LibraryManagementSystem.WebApi.Extensions.ApplicationExtension;
+using Microsoft.OpenApi.Models;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +18,40 @@ builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(config =>
+{
+    config.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Library Management System API",
+        Version = "v1"
+    });
+    config.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter Bearer token"
+    });
+
+    config.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+
+            }, Array.Empty<string>()
+        }
+    });
+});
+
+builder.Services.AddApplicationVersioning();
 
 var app = builder.Build();
 
